b760c6e805986fc13e8e2074ed6fc98f8b537ba1
Nidhekshaa <149925956+Nidhekshaa@users.noreply.github.com>
2025-03-22 18:12:22 +0530

    Update App.java

--- src/main/java/com/github/wololock/App.java
+++ src/main/java/com/github/wololock/App.java
@@ -1,13 +1,11 @@
 package com.github.wololock;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        for(int i=0;i<10;i++){
+            System.out.println( "Hello World!" );
+        }
     }
 }
